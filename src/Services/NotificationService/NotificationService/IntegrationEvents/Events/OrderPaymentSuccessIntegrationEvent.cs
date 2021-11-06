e13670e47158f1eefc2f2faebd4104d3891a8044
kerem <kerem@phonoclick.com>
2021-11-06 17:51:34 +0300

    create notification service

---
+++ src/Services/NotificationService/NotificationService/IntegrationEvents/Events/OrderPaymentSuccessIntegrationEvent.cs
@@ -0,0 +1,12 @@
+﻿using EventBus.Base.Events;
+using System;
+
+namespace NotificationService.IntegrationEvents.Events
+{
+  public class OrderPaymentSuccessIntegrationEvent : IntegrationEvent
+  {
+    public Guid OrderId { get; }
+
+    public OrderPaymentSuccessIntegrationEvent(Guid orderId) => OrderId = orderId;
+  }
+}
