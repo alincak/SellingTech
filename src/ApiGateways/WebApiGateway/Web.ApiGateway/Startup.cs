f72cf8b3b206b671d1669c7d29a14f8e300b3b0a
kerem <kerem@phonoclick.com>
2021-11-27 14:02:21 +0300

    consul discovery service

--- src/ApiGateways/WebApiGateway/Web.ApiGateway/Startup.cs
+++ src/ApiGateways/WebApiGateway/Web.ApiGateway/Startup.cs
@@ -1,16 +1,18 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Web.ApiGateway.Extensions;
+using Web.ApiGateway.Infrastructure;
+using Web.ApiGateway.Services;
+using Web.ApiGateway.Services.Interfaces;
 
 namespace Web.ApiGateway
 {
@@ -26,16 +28,33 @@ namespace Web.ApiGateway
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-
       services.AddControllers();
       services.AddSwaggerGen(c =>
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "Web.ApiGateway", Version = "v1" });
       });
+
+      services.AddScoped<ICatalogService, CatalogService>();
+      services.AddScoped<IBasketService, BasketService>();
+
+      services.ConfigureAuth(Configuration);
+
+      services.AddOcelot().AddConsul();
+
+      ConfigureHttpClient(services);
+
+      services.AddCors(options =>
+      {
+        options.AddPolicy("CorsPolicy",
+            builder => builder.SetIsOriginAllowed((host) => true)
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials());
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       if (env.IsDevelopment())
       {
@@ -47,13 +66,37 @@ namespace Web.ApiGateway
       app.UseHttpsRedirection();
 
       app.UseRouting();
+      app.UseCors("CorsPolicy");
 
+      app.UseAuthentication();
       app.UseAuthorization();
 
       app.UseEndpoints(endpoints =>
       {
         endpoints.MapControllers();
       });
+
+      await app.UseOcelot();
     }
+
+    private void ConfigureHttpClient(IServiceCollection services)
+    {
+      services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+      services.AddTransient<HttpClientDelegatingHandler>();
+
+      services.AddHttpClient("basket", c =>
+      {
+        c.BaseAddress = new Uri(Configuration["urls:basket"]);
+      })
+      .AddHttpMessageHandler<HttpClientDelegatingHandler>()
+      ;
+
+      services.AddHttpClient("catalog", c =>
+      {
+        c.BaseAddress = new Uri(Configuration["urls:catalog"]);
+      })
+      .AddHttpMessageHandler<HttpClientDelegatingHandler>();
+    }
+
   }
 }
